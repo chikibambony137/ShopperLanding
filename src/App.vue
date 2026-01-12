0164e452a2fd67693f3b4fd2025ebeec18fa5c41
Anton <137686641+chikibambony137@users.noreply.github.com>
2026-01-12 22:24:49 +0300

    test container using

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <header
-      class="flex justify-between items-center gap-10 p-12 pl-20 pr-20 bg-white">
+      class=" container mx-auto flex justify-between items-center gap-10 p-12 pl-20 pr-20">
       <h1 class="text-4xl italic">
         <span class="text-[#10b981]">Shop</span>per
       </h1>
@@ -20,138 +20,140 @@
       <a href="/">About</a>
     </nav>
 
-    <article
-      class="grid grid-flow-row justify-center pb-16 xl:grid-cols-[1fr_2fr]">
-      <div class="pt-30 pl-4 xl:pl-20 xl:pr-20 row-end-1">
-        <h2 class="text-6xl xl:w-125 mb-8 font-semibold">
-          Discover Our Latest Products
-        </h2>
-        <p class="text-xl w-130 mb-13 font-medium">
-          Lorem ipsum is a placeholder text commonly used to demonstrate the
-          visual form of a product
-        </p>
-        <button class="mb-28">Buy Now</button>
-      </div>
-
-      <div
-        class="flex justify-center items-center xl:pr-30 xl:pt-20 select-none row-span-2">
-        <img
-          class="w-full max-w-125"
-          src="./assets/RedBeats.png"
-          alt="redbeats"
-          draggable="false" />
-      </div>
-
-      <div class="flex justify-start gap-4 xl:pl-20">
-        <div class="flex select-none">
+    <main class="container mx-auto">
+      <article
+        class="grid grid-flow-row justify-center pb-16 xl:grid-cols-[1fr_2fr]">
+        <div class="pt-30 pl-4 xl:pl-20 xl:pr-20 row-end-1">
+          <h2 class="text-6xl xl:w-125 mb-8 font-semibold">
+            Discover Our Latest Products
+          </h2>
+          <p class="text-xl w-130 mb-13 font-medium">
+            Lorem ipsum is a placeholder text commonly used to demonstrate the
+            visual form of a product
+          </p>
+          <button class="mb-28">Buy Now</button>
+        </div>
+  
+        <div
+          class="flex justify-center items-center xl:pr-30 xl:pt-20 select-none row-span-2">
           <img
-            class="w-15 h-15"
-            src="./assets/ava1.png"
-            alt="ava"
-            draggable="false" />
-          <img
-            class="w-15 h-15 -ml-5"
-            src="./assets/ava2.png"
-            alt="ava"
-            draggable="false" />
-          <img
-            class="w-15 h-15 -ml-5"
-            src="./assets/ava3.png"
-            alt="ava"
-            draggable="false" />
-          <img
-            class="w-15 h-15 -ml-5"
-            src="./assets/ava4.png"
-            alt="ava"
+            class="w-full max-w-125"
+            src="./assets/RedBeats.png"
+            alt="redbeats"
             draggable="false" />
         </div>
-        <p class="w-25 font-medium text-2xl">15k Well Reviews</p>
-      </div>
-    </article>
-
-    <article class="grid grid-cols-[4fr_3fr] bg-[#845ec2] h-150 text-white">
-      <div class="pt-20 pl-24">
-        <h1 class="h-35">
-          <svg class="flex w-full flex-wrap">
-            <text
-              x="0"
-              y="80"
-              font-size="60"
-              font-weight="bold"
-              fill="transparent"
-              stroke="white"
-              stroke-width="1"
-              stroke-linejoin="round"
-              paint-order="stroke">
-              Apple wireless Airpod
-            </text>
-            <text x="0" y="80" font-size="60" font-weight="bold" fill="#845ec2">
-              Apple wireless Airpod
-            </text>
-          </svg>
-        </h1>
-
-        <div class="grid grid-cols-3 w-160 select-none">
-          <img src="./assets/product1.png" alt="product1" draggable="false" />
-          <div class="grid w-48">
+  
+        <div class="flex justify-start gap-4 xl:pl-20">
+          <div class="flex select-none">
             <img
-              class="-translate-y-20"
-              src="./assets/product2.png"
-              alt="product2"
+              class="w-15 h-15"
+              src="./assets/ava1.png"
+              alt="ava"
               draggable="false" />
-            <div class="flex justify-center">
-              <p class="text-2xl">
-                Price: <span class="font-bold"> $45.99</span>
-              </p>
-            </div>
+            <img
+              class="w-15 h-15 -ml-5"
+              src="./assets/ava2.png"
+              alt="ava"
+              draggable="false" />
+            <img
+              class="w-15 h-15 -ml-5"
+              src="./assets/ava3.png"
+              alt="ava"
+              draggable="false" />
+            <img
+              class="w-15 h-15 -ml-5"
+              src="./assets/ava4.png"
+              alt="ava"
+              draggable="false" />
           </div>
-          <img src="./assets/product3.png" alt="product3" draggable="false" />
+          <p class="w-25 font-medium text-2xl">15k Well Reviews</p>
         </div>
-      </div>
-
-      <div class="pt-35">
-        <h2 class="text-5xl mb-6 font-semibold">Our Featured Products</h2>
-        <p class="text-xl w-120 mb-13 font-medium">
-          Lorem ipsum is a placeholder text commonly used to demonstrate the
-          visual form of a product
-        </p>
-        <button class="mr-12">Add to cart</button>
-        <a>View more</a>
-      </div>
-    </article>
-
-    <article class="grid justify-center items-center p-30">
-      <h2 class="text-5xl mb-6 font-semibold">Top Sellers</h2>
-      <div class="flex">
-        <input type="radio" name="type" checked />
-        <p>Top picks</p>
-        <input type="radio" name="type" />
-        <p>Watches</p>
-      </div>
-      <div class="grid grid-cols-3">
-        <Product></Product>
-        <Product></Product>
-        <Product></Product>
-      </div>
-    </article>
-
-    <article class="grid justify-center items-center p-30">
-      <h2 class="text-5xl mb-6 font-semibold">Trending Earphones</h2>
-      <div class="flex">
-        <input type="radio" name="type" checked />
-        <p>Earbuds</p>
-        <input type="radio" name="type" />
-        <p>Wireless</p>
-        <input type="radio" name="type" />
-        <p>Wired</p>
-      </div>
-      <div class="grid grid-cols-3">
-        <Product></Product>
-        <Product></Product>
-        <Product></Product>
-      </div>
-    </article>
-    <article></article>
+      </article>
+  
+      <article class="grid grid-cols-[4fr_3fr] bg-[#845ec2] h-150 text-white">
+        <div class="pt-20 pl-24">
+          <h1 class="h-35">
+            <svg class="flex w-full flex-wrap">
+              <text
+                x="0"
+                y="80"
+                font-size="60"
+                font-weight="bold"
+                fill="transparent"
+                stroke="white"
+                stroke-width="1"
+                stroke-linejoin="round"
+                paint-order="stroke">
+                Apple wireless Airpod
+              </text>
+              <text x="0" y="80" font-size="60" font-weight="bold" fill="#845ec2">
+                Apple wireless Airpod
+              </text>
+            </svg>
+          </h1>
+  
+          <div class="grid grid-cols-3 w-160 select-none">
+            <img src="./assets/product1.png" alt="product1" draggable="false" />
+            <div class="grid w-48">
+              <img
+                class="-translate-y-20"
+                src="./assets/product2.png"
+                alt="product2"
+                draggable="false" />
+              <div class="flex justify-center">
+                <p class="text-2xl">
+                  Price: <span class="font-bold"> $45.99</span>
+                </p>
+              </div>
+            </div>
+            <img src="./assets/product3.png" alt="product3" draggable="false" />
+          </div>
+        </div>
+  
+        <div class="pt-35">
+          <h2 class="text-5xl mb-6 font-semibold">Our Featured Products</h2>
+          <p class="text-xl w-120 mb-13 font-medium">
+            Lorem ipsum is a placeholder text commonly used to demonstrate the
+            visual form of a product
+          </p>
+          <button class="mr-12">Add to cart</button>
+          <a>View more</a>
+        </div>
+      </article>
+  
+      <article class="grid justify-center items-center p-30">
+        <h2 class="text-5xl mb-6 font-semibold">Top Sellers</h2>
+        <div class="flex">
+          <input type="radio" name="type" checked />
+          <p>Top picks</p>
+          <input type="radio" name="type" />
+          <p>Watches</p>
+        </div>
+        <div class="grid grid-cols-3">
+          <Product></Product>
+          <Product></Product>
+          <Product></Product>
+        </div>
+      </article>
+  
+      <article class="grid justify-center items-center p-30">
+        <h2 class="text-5xl mb-6 font-semibold">Trending Earphones</h2>
+        <div class="flex">
+          <input type="radio" name="type" checked />
+          <p>Earbuds</p>
+          <input type="radio" name="type" />
+          <p>Wireless</p>
+          <input type="radio" name="type" />
+          <p>Wired</p>
+        </div>
+        <div class="grid grid-cols-3">
+          <Product></Product>
+          <Product></Product>
+          <Product></Product>
+        </div>
+      </article>
+      <article></article>
+    </main>
     <footer></footer>
   </div>
 </template>
